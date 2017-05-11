f86d28206985579b01b32ce8a4640b123d049ea8
devcfgc <cfgc2828@gmail.com>
2017-05-11 22:02:10 +0200

    Create a simple EC2 instance in aws

--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  access_key = "AWS_ACCESS_KEY_ID"
+  secret_key = "AWS_SECRET_ACCESS_KEY"
+  region     = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-a8d2d7ce"
   instance_type = "t2.micro"
 }
